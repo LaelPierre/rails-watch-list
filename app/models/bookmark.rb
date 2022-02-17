99c63cf03210f5e76a2325dc73af0fb625c8f60c
lael aichaoui <lael.aichaoui@gmail.com>
2022-02-17 10:56:25 -0800

    validation and association done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :movie_id, uniqueness: { scope: :list_id }
-  validates :comment, inclusion: { in: 0..5 }
+  validates :comment, length: { minimum: 6 }
 end
